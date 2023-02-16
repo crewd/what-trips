c6cc59bd9d7662c5f46c94eb671a22667ad6f02c
crewd <newspaper321@nate.com>
2023-02-16 23:34:25 +0900

    feat: token 검증

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,30 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private readonly authService: AuthService) {}
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const authHeader = request.headers['authorization'];
+
+    if (!authHeader) {
+      throw new UnauthorizedException();
+    }
+
+    const token = authHeader.split('Bearer ')[1];
+
+    const payload = this.authService.verify(token);
+    request.user = { userId: payload };
+
+    return true;
+  }
+}
