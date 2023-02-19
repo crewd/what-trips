0955179b93111f22503880538eeb4c05d08b7ec4
crewd <newspaper321@nate.com>
2023-02-19 23:05:27 +0900

    feat: add trip dto 추가

---
+++ src/trip/dto/add-trip.dto.ts
@@ -0,0 +1,24 @@
+import { IsDate, IsString } from 'class-validator';
+
+export class AddTrip {
+  /**
+   * 생성할 여행의 이름
+   * @example '제주도 여행'
+   */
+  @IsString()
+  title: string;
+
+  /**
+   * 여행 시작 날짜
+   * @example '2023/02/15'
+   */
+  @IsDate()
+  startTime: Date;
+
+  /**
+   * 여행 종료 날짜
+   * @example '2023/02/18'
+   */
+  @IsDate()
+  endTime: Date;
+}
