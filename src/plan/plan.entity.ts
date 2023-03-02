6e3e4c99c7b9f32e2992dac60827c63b8c73c278
crewd <newspaper321@nate.com>
2023-03-02 19:32:16 +0900

    feat: plan entity 생성

---
+++ src/plan/plan.entity.ts
@@ -0,0 +1,47 @@
+import { User } from 'src/user/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Trip {
+  @PrimaryColumn()
+  id: number;
+
+  @Column()
+  userId: number;
+
+  @ManyToOne(() => User, (user) => user.id)
+  @JoinColumn({ name: 'userId', referencedColumnName: 'id' })
+  user: User;
+
+  @Column()
+  tripId: number;
+
+  @Column()
+  day: number;
+
+  @Column()
+  startTime: Date;
+
+  @Column()
+  content: string;
+
+  @Column()
+  checked: boolean;
+
+  @Column()
+  coordinate: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
